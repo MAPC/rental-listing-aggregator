cebffe3af6ac34ec6bf51525530ed6f7ca3071b6
Matthew Zagaja <mzagaja@alum.wpi.edu>
2019-10-04 09:39:14 -0400

    Update Deployment IP

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "34.201.61.180", user: "rental-listing-aggregator", roles: %w{app db web}
+server "100.25.148.137", user: "rental-listing-aggregator", roles: %w{app db web}
 
 set :branch, 'develop'
 
