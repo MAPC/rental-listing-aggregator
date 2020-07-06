41e45175052995686f6b024c2af2642c2818ca53
Matthew Zagaja <mzagaja@alum.wpi.edu>
2020-07-06 16:20:58 -0400

    Bump Capistrano Version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.1"
+lock "~> 3.14.1"
 
 set :application, "rental-listing-aggregator"
 set :repo_url, "git@github.com:mapc/rental-listing-aggregator.git"
